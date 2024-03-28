537655b7fcbda97d00439cd5fc79cb258d544e79
rkierbel <rudykierbel@gmail.com>
2024-03-28 16:45:45 +0100

    micronaut app included in verification module

---
+++ verification/src/test/java/com/skilld/VerificationTest.java
@@ -0,0 +1,21 @@
+package com.skilld;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class VerificationTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
